c0752367217d9999d86edc3d3d11aa7b7a9ac0ea
RaiyanHashmi <130068325+RaiyanHashmi@users.noreply.github.com>
2024-07-20 09:51:58 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,17 +3,22 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
-        "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
-        "YAMI~~",
+        "To my",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "Beloved friend",  // 同上...
+        "Today is your special day",
+        "I’m so grateful for you",
+        "Your warmth and understanding",
+        "And the joy you bring into my life",
+        "Your presence is a true blessing",
+        "And your laughter is the melody to my soul",
+        "I cherish every moment we share",
+        "May your day be filled with boundless love",
+        "And your year with endless happiness",
+        "Here’s to many more heartfelt adventures together",
+        "but Remember",
+        "Focus on your ambitions or Goals",
+        "May all your wishes come true",
+        "Happy Birthday !",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
